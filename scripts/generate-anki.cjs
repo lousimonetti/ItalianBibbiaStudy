341e7879260cba6f48a95273e4e95b382f93cb8a
Louis Simonetti <louissimonetti@outlook.com>
2026-04-16 13:46:38 -0400

    fixing memory error for anki

--- scripts/generate-anki.cjs
+++ scripts/generate-anki.cjs
@@ -3,6 +3,7 @@
 // Outputs .apkg files to public/anki/
 
 'use strict';
+global.Module = { TOTAL_MEMORY: 128 * 1024 * 1024 };
 
 const ApkgExport = require('anki-apkg-export').default;
 const fs = require('fs');
